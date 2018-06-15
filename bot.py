1a4e6da3ca3a5700695464e970ca1f77dd470236
Alexander Lo <alexander.lo@utexas.edu>
2018-06-15 03:07:42 -0500

    Updated bot framework from asyncio to rewrite

--- bot.py
+++ bot.py
@@ -1,32 +1,26 @@
-import datetime
-import json
-import requests
-
-import asyncio
 import discord
 from discord.ext import commands
 
 import settings.config
 
-client = discord.Client()
+bot = commands.Bot(command_prefix=settings.config.PREFIX)
 
-@client.event
+@bot.event
 async def on_ready():
     print('Logged in...')
-    print('Username: ' + str(client.user.name))
-    print('Client ID: ' + str(client.user.id))
-    print('Invite URL: ' + 'https://discordapp.com/oauth2/authorize?&client_id=' + client.user.id + '&scope=bot&permissions=0')
+    print('Username: ' + str(bot.user.name))
+    print('Client ID: ' + str(bot.user.id))
+    print('Invite URL: ' + 'https://discordapp.com/oauth2/authorize?&client_id=' + bot.user.id + '&scope=bot&permissions=0')
     
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-    
-    if message.content.startswith(settings.config.PREFIX):
-        message.content = message.content[1:].lower()
-        
-    if message.content.startswith('ping'):
-        msg = 'Pong!'
-        await client.send_message(message.channel, msg)
-    
-client.run(settings.config.TOKEN)
+@commands.command()
+async def ping():
+    await bot.say('Pong!')
+
+@commands.command()
+async def shutdown():    
+    await bot.logout()
+    await bot.close()
+
+bot.add_command(ping)
+bot.add_command(shutdown)    
+bot.run(settings.config.TOKEN)
