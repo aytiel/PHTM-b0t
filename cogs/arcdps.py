ebf16ca948a955c06d0b101d038b802d9b69d89e
Alexander Lo <alexander.lo@utexas.edu>
2018-06-17 01:14:22 -0500

    Condensed two dictionaries into one

--- cogs/arcdps.py
+++ cogs/arcdps.py
@@ -6,43 +6,45 @@ import os
 
 import discord
 from discord.ext import commands
+import settings.config
 
-raid_logs = {
-    'W1': {
-        'Vale Guardian': None,
-        'Gorseval the Multifarious': None,
-        'Sabetha the Saboteur': None
+logs = {
+    'raids': {
+        'W1': {
+            'Vale Guardian': {'dps.report': None, 'GW2Raidar': None},
+            'Gorseval the Multifarious': {'dps.report': None, 'GW2Raidar': None},
+            'Sabetha the Saboteur': {'dps.report': None, 'GW2Raidar': None}
+        },
+        'W2': {
+            'Slothasor': {'dps.report': None, 'GW2Raidar': None},
+            'Matthias Gabrel': {'dps.report': None, 'GW2Raidar': None}
+        },
+        'W3': {
+            'Keep Construct': {'dps.report': None, 'GW2Raidar': None},
+            'Xera': {'dps.report': None, 'GW2Raidar': None}
+        },
+        'W4': {
+            'Cairn the Indomitable': {'dps.report': None, 'GW2Raidar': None},
+            'Mursaat Overseer': {'dps.report': None, 'GW2Raidar': None},
+            'Samarog': {'dps.report': None, 'GW2Raidar': None},
+            'Deimos': {'dps.report': None, 'GW2Raidar': None}
+        },
+        'W5': {
+            'Soulless Horror': {'dps.report': None, 'GW2Raidar': None},
+            'Dhuum': {'dps.report': None, 'GW2Raidar': None}
+        }
     },
-    'W2': {
-        'Slothasor': None,
-        'Matthias Gabrel': None
-    },
-    'W3': {
-        'Keep Construct': None,
-        'Xera': None
-    },
-    'W4': {
-        'Cairn the Indomitable': None,
-        'Mursaat Overseer': None,
-        'Samarog': None,
-        'Deimos': None
-    },
-    'W5': {
-        'Soulless Horror': None,
-        'Dhuum': None
-    }
-}
-
-frac_logs = {
-    '99CM': {
-        'MAMA': None,
-        'Nightmare Oratuss': None,
-        'Ensolyss of the Endless Torment': None
-    },
-    '100CM': {
-        'Skorvald the Shattered': None,
-        'Artsariiv': None,
-        'Arkk': None
+    'fractals': {
+        '99CM': {
+            'MAMA': {'dps.report': None, 'GW2Raidar': None},
+            'Nightmare Oratuss': {'dps.report': None, 'GW2Raidar': None},
+            'Ensolyss of the Endless Torment': {'dps.report': None, 'GW2Raidar': None}
+        },
+        '100CM': {
+            'Skorvald the Shattered': {'dps.report': None, 'GW2Raidar': None},
+            'Artsariiv': {'dps.report': None, 'GW2Raidar': None},
+            'Arkk': {'dps.report': None, 'GW2Raidar': None}
+        }
     }
 }
 
@@ -71,52 +73,28 @@ class Arcdps:
         title = '__' + name + '__'
         await ctx.send(title)
         
-        if type == 'raids':
-            for w in raid_logs:
-                wing = w + ':'
-                await ctx.send(wing)
-                for b in raid_logs[w]:
-                    path = 'C:/Users/atl/Documents/Guild Wars 2/addons/arcdps/arcdps.cbtlogs/' + b + '/*'
-                    all_files = glob.glob(path)
-                    latest_file = max(all_files, key=os.path.getctime)
-                    if latest_file is None:
-                        raid_logs[w][b] = None
+        for e in logs[type]:
+            encount = e + ':'
+            await ctx.send(encount)
+            for b in logs[type][e]:
+                path = 'C:/Users/atl/Documents/Guild Wars 2/addons/arcdps/arcdps.cbtlogs/' + b + '/*'
+                all_files = glob.glob(path)
+                latest_file = max(all_files, key=os.path.getctime)
+                if latest_file is None:
+                    logs[type][e][b]['dps.report'] = None
                         
-                    dps_endpoint = 'https://dps.report/uploadContent?json=1&generator=rh'
-                    files = {'file': open(latest_file, 'rb')}
-                    res = requests.post(dps_endpoint, files=files)
-                    if not res.status_code == 200:
-                        raid_logs[w][b] = None
-                    else:
-                        raid_logs[w][b] = json.loads(res.text)
+                dps_endpoint = 'https://dps.report/uploadContent?json=1&generator=rh'
+                files = {'file': open(latest_file, 'rb')}
+                res = requests.post(dps_endpoint, files=files)
+                if not res.status_code == 200:
+                    logs[type][e][b]['dps.report'] = None
+                else:
+                    logs[type][e][b]['dps.report'] = res.json()['permalink']
                     
-                    if raid_logs[w][b] is None:
-                        await ctx.send('ERROR 🤖')
-                    else:
-                        await ctx.send(raid_logs[w][b]['permalink'])
-        else:
-            for s in frac_logs:
-                scale = s + ':'
-                await ctx.send(scale)
-                for b in frac_logs[s]:
-                    path = 'C:/Users/atl/Documents/Guild Wars 2/addons/arcdps/arcdps.cbtlogs/' + b + '/*'
-                    all_files = glob.glob(path)
-                    latest_file = max(all_files, key=os.path.getctime)
-                    if latest_file is None:
-                        frac_logs[s][b] = None
-                        
-                    dps_endpoint = 'https://dps.report/uploadContent?json=1&rotation_weap1=0&generator=rh'
-                    files = {'file': open(latest_file, 'rb')}
-                    res = requests.post(dps_endpoint, files=files)
-                    if not res.status_code == 200:
-                        frac_logs[s][b] = None
-                    else:
-                        frac_logs[s][b] = json.loads(res.text)
-                    
-                    if frac_logs[s][b] is None:
-                        await ctx.send('ERROR 🤖')
-                    else:
-                        await ctx.send(frac_logs[s][b]['permalink'])
+                if logs[type][e][b]['dps.report'] is None:
+                    await ctx.send('ERROR 🤖')
+                else:
+                    await ctx.send(logs[type][e][b]['dps.report'])
         
 def setup(bot):
     bot.add_cog(Arcdps(bot))
