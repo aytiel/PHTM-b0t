9f09a84e2898562240e9915d299247732ebad0aa
Alexander Lo <alexander.lo@utexas.edu>
2018-06-18 16:09:45 -0500

    Implemented uploading to GW2Raidar. Displaying still not working.

--- cogs/arcdps.py
+++ cogs/arcdps.py
@@ -1,6 +1,7 @@
 import json
 import requests
 import datetime
+import time
 import glob
 import os
 
@@ -11,39 +12,39 @@ import settings.config
 logs = {
     'raids': {
         'W1': {
-            'Vale Guardian': {'dps.report': None, 'GW2Raidar': None},
-            'Gorseval the Multifarious': {'dps.report': None, 'GW2Raidar': None},
-            'Sabetha the Saboteur': {'dps.report': None, 'GW2Raidar': None}
+            'Vale Guardian': {'dps.report': 'about:blank', 'GW2Raidar': 'about:blank'},
+            'Gorseval the Multifarious': {'dps.report': 'about:blank', 'GW2Raidar': 'about:blank'},
+            'Sabetha the Saboteur': {'dps.report': 'about:blank', 'GW2Raidar': 'about:blank'}
         },
         'W2': {
-            'Slothasor': {'dps.report': None, 'GW2Raidar': None},
-            'Matthias Gabrel': {'dps.report': None, 'GW2Raidar': None}
+            'Slothasor': {'dps.report': 'about:blank', 'GW2Raidar': 'about:blank'},
+            'Matthias Gabrel': {'dps.report': 'about:blank', 'GW2Raidar': 'about:blank'}
         },
         'W3': {
-            'Keep Construct': {'dps.report': None, 'GW2Raidar': None},
-            'Xera': {'dps.report': None, 'GW2Raidar': None}
+            'Keep Construct': {'dps.report': 'about:blank', 'GW2Raidar': 'about:blank'},
+            'Xera': {'dps.report': 'about:blank', 'GW2Raidar': 'about:blank'}
         },
         'W4': {
-            'Cairn the Indomitable': {'dps.report': None, 'GW2Raidar': None},
-            'Mursaat Overseer': {'dps.report': None, 'GW2Raidar': None},
-            'Samarog': {'dps.report': None, 'GW2Raidar': None},
-            'Deimos': {'dps.report': None, 'GW2Raidar': None}
+            'Cairn the Indomitable': {'dps.report': 'about:blank', 'GW2Raidar': 'about:blank'},
+            'Mursaat Overseer': {'dps.report': 'about:blank', 'GW2Raidar': 'about:blank'},
+            'Samarog': {'dps.report': 'about:blank', 'GW2Raidar': 'about:blank'},
+            'Deimos': {'dps.report': 'about:blank', 'GW2Raidar': 'about:blank'}
         },
         'W5': {
-            'Soulless Horror': {'dps.report': None, 'GW2Raidar': None},
-            'Dhuum': {'dps.report': None, 'GW2Raidar': None}
+            'Soulless Horror': {'dps.report': 'about:blank', 'GW2Raidar': 'about:blank'},
+            'Dhuum': {'dps.report': 'about:blank', 'GW2Raidar': 'about:blank'}
         }
     },
     'fractals': {
         '99CM': {
-            'MAMA': {'dps.report': None, 'GW2Raidar': None},
-            'Nightmare Oratuss': {'dps.report': None, 'GW2Raidar': None},
-            'Ensolyss of the Endless Torment': {'dps.report': None, 'GW2Raidar': None}
+            'MAMA': {'dps.report': 'about:blank', 'GW2Raidar': 'about:blank'},
+            'Nightmare Oratuss': {'dps.report': 'about:blank', 'GW2Raidar': 'about:blank'},
+            'Ensolyss of the Endless Torment': {'dps.report': 'about:blank', 'GW2Raidar': 'about:blank'}
         },
         '100CM': {
-            'Skorvald the Shattered': {'dps.report': None, 'GW2Raidar': None},
-            'Artsariiv': {'dps.report': None, 'GW2Raidar': None},
-            'Arkk': {'dps.report': None, 'GW2Raidar': None}
+            'Skorvald the Shattered': {'dps.report': 'about:blank', 'GW2Raidar': 'about:blank'},
+            'Artsariiv': {'dps.report': 'about:blank', 'GW2Raidar': 'about:blank'},
+            'Arkk': {'dps.report': 'about:blank', 'GW2Raidar': 'about:blank'}
         }
     }
 }
@@ -74,31 +75,87 @@ class Arcdps:
                 all_files = glob.glob(path)
                 latest_file = max(all_files, key=os.path.getctime)
                 if latest_file is None:
-                    logs[type][e][b]['dps.report'] = None
-                files = {'file': open(latest_file, 'rb')}
+                    error = 'ERROR ?? : an error has occurred with ' + b
+                    await ctx.send(error)
+                    continue
                         
                 dps_endpoint = 'https://dps.report/uploadContent?json=1&generator=rh'
-                res = requests.post(dps_endpoint, files=files)
+                with open(latest_file, 'rb') as file:
+                    files = {'file': file}
+                    res = requests.post(dps_endpoint, files=files)
+                    if not res.status_code == 200:
+                        error = 'ERROR ?? : an error has occurred with ' + b
+                        await ctx.send(error)
+                        continue
+                    else:
+                        logs[type][e][b]['dps.report'] = res.json()['permalink']
+                print('Uploaded ' + b + ': dps.report')
+                    
+                raidar_endpoint = 'https://www.gw2raidar.com/api/v2/token'
+                cred = {'username': settings.config.RAIDARUSER, 'password': settings.config.RAIDARPASS}
+                res = requests.post(raidar_endpoint, data=cred)
                 if not res.status_code == 200:
-                    logs[type][e][b]['dps.report'] = 'ERROR 🤖'
+                    error = 'ERROR ?? : an error has occurred with ' + b
+                    await ctx.send(error)
+                    continue
                 else:
-                    logs[type][e][b]['dps.report'] = res.json()['permalink']
-                print('Uploaded ' + b)
-                    
+                    token = res.json()['token']
+                    auth = 'Token ' + token
+                    raidar_endpoint = 'https://www.gw2raidar.com/api/v2/encounters/new'
+                    with open(latest_file, 'rb') as file:
+                        files = {'file': file}
+                        res = requests.put(raidar_endpoint, headers={'Authorization': auth}, files=files)
+                        if not res.status_code == 200:
+                            error = 'ERROR ?? : an error has occurred with ' + b
+                            await ctx.send(error)
+                            continue
+                        else:
+                            raidar_endpoint = 'https://www.gw2raidar.com/api/v2/areas'
+                            res = requests.get(raidar_endpoint, headers={'Authorization': auth})
+                            if not res.status_code == 200:
+                                error = 'ERROR ?? : an error has occurred with ' + b
+                                await ctx.send(error)
+                                continue
+                            #else:
+                            #    boss_id = None
+                            #    for boss in res.json()['results']:
+                            #        if boss['name'].split(' ')[0] == 'Siax' and b == 'Nightmare Oratuss':
+                            #            boss_id = boss['id']
+                            #            break
+                            #        elif boss['name'].split(' ')[0] in b:
+                            #            boss_id = boss['id']
+                            #            break
+                            #    if boss_id is None:
+                            #        error = 'ERROR ?? : an error has occurred with ' + b
+                            #        await ctx.send(error)
+                            #        continue
+                            #    counter = 0
+                            #    await self.update_raidar(ctx, type, e, b, auth, boss_id, counter)
+                print('Uploaded ' + b + ': GW2Raidar')          
+                
         await self.print_logs(ctx, type, name)
-                    
-                #raidar_endpoint = 'https://www.gw2raidar.com/api/v2/token'
-                #cred = {'username': settings.config.RAIDARUSER, 'password': settings.config.RAIDARPASS}
-                #res = requests.post(raidar_endpoint, data=cred)
-                #if not res.status_code == 200:
-                #    logs[type][e][b]['GW2Raidar'] = 'ERROR 🤖'
-                #else:
-                #    token = res.json()['token']
-                #    auth = 'Token ' + token
-                #    raidar_endpoint = 'https://www.gw2raidar.com/api/v2/encounters/new'
-                #    res = requests.put(raidar_endpoint, headers={'Authorization': auth}, files=files)
-                #    print(res)
-                #    print(res.text)
+        
+    async def update_raidar(self, ctx, type: str, e: str, b: str, auth: str, boss_id: int, counter: int):
+        raidar_endpoint = 'https://www.gw2raidar.com/api/v2/encounters?limit=1'
+        res = requests.get(raidar_endpoint, headers={'Authorization': auth})
+        if not res.status_code == 200:
+            error = 'ERROR ?? : an error has occurred with ' + b
+            await ctx.send(error)
+            return
+        else:
+            if res.json()['results'][0]['area_id'] == boss_id:
+                raidar_link = 'https://www.gw2raidar.com/encounter/' + res.json()['results'][0]['url_id']
+                logs[type][e][b]['GW2Raidar'] = raidar_link
+                return
+            elif counter == 3:
+                error = 'ERROR ?? : an error has occurred with ' + b
+                await ctx.send(error)
+                return
+            else:
+                print(b + ': GW2Raidar has not been retrieved. Retrying ' + str(counter))
+                time.sleep(40)
+                counter += 1
+                await self.update_raidar(ctx, type, e, b, auth, boss_id, counter)       
                     
     async def print_logs(self, ctx, type: str, name: str):
         title = '__' + name + ' | ' + str(datetime.date.today()) + '__'
@@ -108,7 +165,6 @@ class Arcdps:
         for e in logs[type]:
             out = ''
             name = e + ':'
-            count = 0
             for b in logs[type][e]:
                 boss = b
                 if boss == 'Nightmare Oratuss':
@@ -118,10 +174,7 @@ class Arcdps:
                     if len(split) > 1:
                         if split[1] == 'the' or split[1] == 'of' or split[1] == 'Gabrel':
                             boss = split[0]
-                out += '[' + boss + '](' + logs[type][e][b]['dps.report'] + ')  '
-                if not count == len(logs[type][e])-1:
-                    out += '|  '
-                count += 1
+                out += boss + '  |  ' + '[dps.report](' + logs[type][e][b]['dps.report'] + ')  |  [GW2Raidar](' + logs[type][e][b]['GW2Raidar'] + ')\n'
             embed.add_field(name=name, value=out, inline=False)
             
         await ctx.send(embed=embed)
--- settings/config.py
+++ settings/config.py
@@ -1,3 +1,5 @@
 TOKEN = 'NDU3MDUzMDQ0NjMxMTQyNDAx.DgTfkA.ADDu79ZtpPjALZMi9V1Vh6YyfJw'
-
 PREFIX = '!'
+
+RAIDARUSER = 'KrimsonPhantom'
+RAIDARPASS = 'Alo231Lionking0'
