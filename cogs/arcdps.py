9b2d6b03f859f5d1f60f5306a26c50551827bc73
Alexander Lo <alexander.lo@utexas.edu>
2018-06-25 16:26:24 -0500

    Made boss_id statically hardcoded to reduce number of requests

--- cogs/arcdps.py
+++ cogs/arcdps.py
@@ -12,39 +12,39 @@ import settings.config
 logs = {
     'raids': {
         'W1': {
-            'Vale Guardian': {'dps.report': 'about:blank', 'GW2Raidar': 'about:blank'},
-            'Gorseval the Multifarious': {'dps.report': 'about:blank', 'GW2Raidar': 'about:blank'},
-            'Sabetha the Saboteur': {'dps.report': 'about:blank', 'GW2Raidar': 'about:blank'}
+            'Vale Guardian': {'dps.report': 'about:blank', 'GW2Raidar': {'id': 15438, 'link': 'about:blank'}},
+            'Gorseval the Multifarious': {'dps.report': 'about:blank', 'GW2Raidar': {'id': 15429, 'link': 'about:blank'}},
+            'Sabetha the Saboteur': {'dps.report': 'about:blank', 'GW2Raidar': {'id': 15375, 'link': 'about:blank'}}
         },
         'W2': {
-            'Slothasor': {'dps.report': 'about:blank', 'GW2Raidar': 'about:blank'},
-            'Matthias Gabrel': {'dps.report': 'about:blank', 'GW2Raidar': 'about:blank'}
+            'Slothasor': {'dps.report': 'about:blank', 'GW2Raidar': {'id': 16123, 'link': 'about:blank'}},
+            'Matthias Gabrel': {'dps.report': 'about:blank', 'GW2Raidar': {'id': 16115, 'link': 'about:blank'}}
         },
         'W3': {
-            'Keep Construct': {'dps.report': 'about:blank', 'GW2Raidar': 'about:blank'},
-            'Xera': {'dps.report': 'about:blank', 'GW2Raidar': 'about:blank'}
+            'Keep Construct': {'dps.report': 'about:blank', 'GW2Raidar': {'id': 16235, 'link': 'about:blank'}},
+            'Xera': {'dps.report': 'about:blank', 'GW2Raidar': {'id': 16246, 'link': 'about:blank'}}
         },
         'W4': {
-            'Cairn the Indomitable': {'dps.report': 'about:blank', 'GW2Raidar': 'about:blank'},
-            'Mursaat Overseer': {'dps.report': 'about:blank', 'GW2Raidar': 'about:blank'},
-            'Samarog': {'dps.report': 'about:blank', 'GW2Raidar': 'about:blank'},
-            'Deimos': {'dps.report': 'about:blank', 'GW2Raidar': 'about:blank'}
+            'Cairn the Indomitable': {'dps.report': 'about:blank', 'GW2Raidar': {'id': 17194, 'link': 'about:blank'}},
+            'Mursaat Overseer': {'dps.report': 'about:blank', 'GW2Raidar': {'id': 17172, 'link': 'about:blank'}},
+            'Samarog': {'dps.report': 'about:blank', 'GW2Raidar': {'id': 17188, 'link': 'about:blank'}},
+            'Deimos': {'dps.report': 'about:blank', 'GW2Raidar': {'id': 17154, 'link': 'about:blank'}}
         },
         'W5': {
-            'Soulless Horror': {'dps.report': 'about:blank', 'GW2Raidar': 'about:blank'},
-            'Dhuum': {'dps.report': 'about:blank', 'GW2Raidar': 'about:blank'}
+            'Soulless Horror': {'dps.report': 'about:blank', 'GW2Raidar': {'id': 19767, 'link': 'about:blank'}},
+            'Dhuum': {'dps.report': 'about:blank', 'GW2Raidar': {'id': 19450, 'link': 'about:blank'}}
         }
     },
     'fractals': {
         '99CM': {
-            'MAMA': {'dps.report': 'about:blank', 'GW2Raidar': 'about:blank'},
-            'Nightmare Oratuss': {'dps.report': 'about:blank', 'GW2Raidar': 'about:blank'},
-            'Ensolyss of the Endless Torment': {'dps.report': 'about:blank', 'GW2Raidar': 'about:blank'}
+            'MAMA': {'dps.report': 'about:blank', 'GW2Raidar': {'id': 17021, 'link': 'about:blank'}},
+            'Nightmare Oratuss': {'dps.report': 'about:blank', 'GW2Raidar': {'id': 17028, 'link': 'about:blank'}},
+            'Ensolyss of the Endless Torment': {'dps.report': 'about:blank', 'GW2Raidar': {'id': 16948, 'link': 'about:blank'}}
         },
         '100CM': {
-            'Skorvald the Shattered': {'dps.report': 'about:blank', 'GW2Raidar': 'about:blank'},
-            'Artsariiv': {'dps.report': 'about:blank', 'GW2Raidar': 'about:blank'},
-            'Arkk': {'dps.report': 'about:blank', 'GW2Raidar': 'about:blank'}
+            'Skorvald the Shattered': {'dps.report': 'about:blank', 'GW2Raidar': {'id': 17632, 'link': 'about:blank'}},
+            'Artsariiv': {'dps.report': 'about:blank', 'GW2Raidar': {'id': 17949, 'link': 'about:blank'}},
+            'Arkk': {'dps.report': 'about:blank', 'GW2Raidar': {'id': 17759, 'link': 'about:blank'}}
         }
     }
 }
@@ -124,32 +124,13 @@ class Arcdps:
                             await ctx.send(error)
                             continue
                         else:
-                            raidar_endpoint = 'https://www.gw2raidar.com/api/v2/areas'
-                            res = requests.get(raidar_endpoint, headers={'Authorization': auth})
-                            if not res.status_code == 200:
-                                error = 'ERROR :robot: : an error has occurred with ' + b
-                                await ctx.send(error)
-                                continue
-                            else:
-                                boss_id = None
-                                for boss in res.json()['results']:
-                                    if boss['name'].split(' ')[0] == 'Siax' and b == 'Nightmare Oratuss':
-                                        boss_id = boss['id']
-                                        break
-                                    elif boss['name'].split(' ')[0] in b:
-                                        boss_id = boss['id']
-                                        break
-                                if boss_id is None:
-                                    error = 'ERROR :robot: : an error has occurred with ' + b
-                                    await ctx.send(error)
-                                    continue
-                                counter = 0
-                                await self.update_raidar(ctx, type, e, b, auth, boss_id, counter)
+                            counter = 0
+                            await self.update_raidar(ctx, type, e, b, auth, counter)
                 print('Uploaded ' + b + ': GW2Raidar')          
                 
         await self.print_logs(ctx, type, name)
         
-    async def update_raidar(self, ctx, type: str, e: str, b: str, auth: str, boss_id: int, counter: int):
+    async def update_raidar(self, ctx, type: str, e: str, b: str, auth: str, counter: int):
         raidar_endpoint = 'https://www.gw2raidar.com/api/v2/encounters?limit=1'
         res = requests.get(raidar_endpoint, headers={'Authorization': auth})
         if not res.status_code == 200:
@@ -157,9 +138,9 @@ class Arcdps:
             await ctx.send(error)
             return
         else:
-            if res.json()['results'][0]['area_id'] == boss_id:
+            if res.json()['results'][0]['area_id'] == logs[type][e][b]['GW2Raidar']['id']:
                 raidar_link = 'https://www.gw2raidar.com/encounter/' + res.json()['results'][0]['url_id']
-                logs[type][e][b]['GW2Raidar'] = raidar_link
+                logs[type][e][b]['GW2Raidar']['link'] = raidar_link
                 return
             elif counter == 3:
                 error = 'ERROR :robot: : ' + b + ' is not finished analyzing.'
@@ -169,17 +150,21 @@ class Arcdps:
                 print(b + ': GW2Raidar has not been analyzed. Retrying ' + str(counter))
                 time.sleep(100)
                 counter += 1
-                await self.update_raidar(ctx, type, e, b, auth, boss_id, counter)       
+                await self.update_raidar(ctx, type, e, b, auth, counter)       
                     
     async def print_logs(self, ctx, type: str, name: str):
         title = '__' + name + ' | ' + str(datetime.date.today()) + '__'
         embed = discord.Embed(title=title, colour=0xb30000)
-        embed.set_footer(text='Created by Phantom#4985')
+        embed.set_footer(text='Direct any issues to Phantom#4985 or PhantomSoulz.2419')
         embed.set_thumbnail(url='https://vignette.wikia.nocookie.net/gwwikia/images/4/4d/Guild_Wars_2_Dragon_logo.jpg/revision/latest?cb=20090825055046')
         for e in logs[type]:
             out = ''
             name = e + ':'
+            no_link = 0
             for b in logs[type][e]:
+                if logs[type][e][b]['dps.report'] == 'about:blank' and logs[type][e][b]['GW2Raidar']['link'] == 'about:blank':
+                    no_link += 1
+                    continue
                 boss = b
                 if boss == 'Nightmare Oratuss':
                     boss = 'Siax'
@@ -188,7 +173,9 @@ class Arcdps:
                     if len(split) > 1:
                         if split[1] == 'the' or split[1] == 'of' or split[1] == 'Gabrel':
                             boss = split[0]
-                out += boss + '  |  ' + '[dps.report](' + logs[type][e][b]['dps.report'] + ')  |  [GW2Raidar](' + logs[type][e][b]['GW2Raidar'] + ')\n'
+                out += boss + '  |  ' + '[dps.report](' + logs[type][e][b]['dps.report'] + ')  |  [GW2Raidar](' + logs[type][e][b]['GW2Raidar']['link'] + ')\n'
+            if no_link == len(logs[type][e]):
+                continue
             embed.add_field(name=name, value=out, inline=False)
             
         await ctx.send(embed=embed)
