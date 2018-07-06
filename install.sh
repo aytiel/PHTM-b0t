5559296be71c4653faf4151a74978197130106a0
Alexander Lo <alexander.lo@utexas.edu>
2018-07-05 22:12:40 -0500

    Added some error checking to install script

--- install.sh
+++ install.sh
@@ -14,11 +14,26 @@ fi
 
 echo "${green}Updating pip...${reset}"
 python -m pip install --upgrade pip
+if [ $? == "0" ]; then
+    echo "${green}pip successfully updated.${reset}"
+else
+    echo "${red}An error has occurred : pip.${reset}"
+fi
 
 echo "${green}Installing dependencies through requirements...${reset}"
 python -m pip install -r requirements.txt
+if [ $? == "0" ]; then
+    echo "${green}requirements successfully installed.${reset}"
+else
+    echo "${red}An error has occurred : requirements.${reset}"
+fi
 
 echo "${green}Installing discord.py...${reset}"
 python -m pip install -U discord.py[voice]
+if [ $? == "0" ]; then
+    echo "${green}discord.py successfully installed.${reset}"
+else
+    echo "${red}An error has occurred : discord.py.${reset}"
+fi
 
 read -n 1 -s -r -p "${green}Press any key to continue...${reset}"
