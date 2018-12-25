04bad7db993cf7e37daeddd2f32b209f57dddb65
Alexander Lo <alexander.lo@utexas.edu>
2018-12-24 22:28:51 -0600

    Added Trio

--- settings/version.py
+++ settings/version.py
@@ -1,5 +1,5 @@
-MAIN_VERSION = '1.1.4'
-SUB_VERSION = 'z'
+MAIN_VERSION = '1.1.5'
+SUB_VERSION = ''
 VERSION = MAIN_VERSION + SUB_VERSION
 
-CHANGE_LOG = '* Adjusted "--time" for dps.report with the GW2EI layout update.\n    - Please ensure that you have either Chrome or Firefox installed.\n    - Please run the install.sh script. The bot will not work until you do.\n* Fixed some miscellaneous formatting bugs.\n* Added support for new .zevtc file extension.\n'
+CHANGE_LOG = '* Added Trio to the boss upload list.\n'
