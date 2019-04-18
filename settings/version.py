54f03da82695d3782669647cf50b8a1e132457b7
Alexander Lo <alexander.lo@utexas.edu>
2019-04-18 13:15:02 -0500

    Added statues event

--- settings/version.py
+++ settings/version.py
@@ -1,5 +1,5 @@
-MAIN_VERSION = '1.1.5'
-SUB_VERSION = 's2'
+MAIN_VERSION = '1.1.6'
+SUB_VERSION = ''
 VERSION = MAIN_VERSION + SUB_VERSION
 
-CHANGE_LOG = '* Added Spanish support.\n* Deprecated the "--aa" flag due to auto attacks being toggleable.\n* Updated class inheritance to match new discord.py format.*\n  - Please run the "install.sh" file if the bot does not seem to be working.'
+CHANGE_LOG = '* Added W5 Statues events.'
