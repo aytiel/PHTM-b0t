f6ed567f4a3030bebc37bb7fa239430ed53ac6a1
Alexander Lo <alexander.lo@utexas.edu>
2020-04-24 23:15:56 -0500

    Added Twisted Castle

--- settings/version.py
+++ settings/version.py
@@ -1,5 +1,5 @@
-MAIN_VERSION = '1.1.8a'
+MAIN_VERSION = '1.1.9'
 SUB_VERSION = ''
 VERSION = MAIN_VERSION + SUB_VERSION
 
-CHANGE_LOG = '* Fixed German translation for Qadim 2.\n* Language no longer needs to be manually selected.\n* dps.report \'--time\' flag revamped from data scraping to API.'
+CHANGE_LOG = '* Added Twisted Castle.'
