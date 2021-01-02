9582181ac7cb29c54e3ed6f6ef984c5348c4d9eb
Alex Lo <alo231@yahoo.com>
2021-01-02 00:10:09 -0600

    Added new 100CM and adjusted old CMs

--- settings/version.py
+++ settings/version.py
@@ -1,5 +1,5 @@
-MAIN_VERSION = '1.1.9'
-SUB_VERSION = 'e'
+MAIN_VERSION = '1.2.0'
+SUB_VERSION = ''
 VERSION = MAIN_VERSION + SUB_VERSION
 
-CHANGE_LOG = '* Changed Eye of Judgment to Statue of Darkness.\n* Added Eye of Fate to folder detection for Statue of Darkness.'
+CHANGE_LOG = '* Added support for Sunqua Peak CM.\n* Adjusted Nightmare and Shattered Observatory fractal numbers to 98 and 99.'
